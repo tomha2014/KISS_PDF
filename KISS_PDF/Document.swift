3a593ea4b3c0d70cc33b12eb6ec9e6e88c8dc167
tom <tomhackbarth@gmail.com>
2020-11-12 14:36:53 -0500

    Initial Commit

---
+++ KISS_PDF/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  KISS_PDF
+//
+//  Created by tom hackbarth on 11/12/20.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ KISS_PDFTests/KISS_PDFTests.swift
@@ -0,0 +1,33 @@
+//
+//  KISS_PDFTests.swift
+//  KISS_PDFTests
+//
+//  Created by tom hackbarth on 11/12/20.
+//
+
+import XCTest
+@testable import KISS_PDF
+
+class KISS_PDFTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
