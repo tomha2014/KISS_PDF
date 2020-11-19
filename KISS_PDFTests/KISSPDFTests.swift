802074e442a2ee050f3ff6ab12712bc4ce50da27
tomhackbarth <tomhackbarth@gmail.com>
2020-11-19 14:10:30 -0500

    updated with swiftLint

--- KISS_PDFTests/KISSPDFTests.swift
+++ KISS_PDFTests/KISSPDFTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import KISS_PDF
 
-class KISS_PDFTests: XCTestCase {
+class KISSPDFTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
